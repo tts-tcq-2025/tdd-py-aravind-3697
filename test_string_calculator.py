c012bd79124e46bf6089ddaff8beee23681cf896
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-27 19:37:00 +0530

    Update test_string_calculator.py

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,3 +4,8 @@ from string_calculator import StringCalculator
 def test_empty_string_returns_zero():
     calc = StringCalculator()
     assert calc.add("") == 0
+
+def test_single_number():
+    calc = StringCalculator()
+    assert calc.add("5") == 5
+
