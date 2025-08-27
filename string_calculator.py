6ba80a21153035e21730b02468bce20e412daf78
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-27 19:39:45 +0530

    Update string_calculator.py

--- string_calculator.py
+++ string_calculator.py
@@ -2,4 +2,5 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
-        return int(numbers)
+        nums = numbers.split(",")
+        return sum(int(n) for n in nums)
