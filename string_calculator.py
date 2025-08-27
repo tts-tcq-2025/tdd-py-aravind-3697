592000e9a24726ec30cf7efb2ef8f56c358a949c
aravind-3697 <43001827+aravind-3697@users.noreply.github.com>
2025-08-27 20:41:06 +0530

    Update string_calculator.py

--- string_calculator.py
+++ string_calculator.py
@@ -2,6 +2,11 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = parts[0][2:]
+            numbers = parts[1].replace(delimiter, ",")
         numbers = numbers.replace("\n", ",")
         nums = numbers.split(",")
         return sum(int(n) for n in nums)
+        
